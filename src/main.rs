c770a07c5db15212df6e7b373e2e6b276211ea6d
Illia Katerynych <iliakat.kr@gmail.com>
2024-05-24 16:44:46 +0100

    gui implementation attempt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![windows_subsystem = "windows"]
 
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+
 fn convert_image(input_file: String, ext: &str) {
     // Get the name of the image file
     let new_file_path: &str = input_file.split(".").collect::<Vec<&str>>()[0];
@@ -19,14 +23,37 @@ fn convert_image(input_file: String, ext: &str) {
     };
 }
 
-fn main() {
+fn main() -> glib::ExitCode{
     // Get the command line arguments
-    let args: Vec<String> = std::env::args().collect();
+    // let args: Vec<String> = std::env::args().collect();
 
-    let input_file = args[1].to_string();
-    let input_file = input_file.trim();
+    // let input_file = args[1].to_string();
+    // let input_file = input_file.trim();
 
-    let ext: &str = ".png";
+    // let ext: &str = ".png";
 
-    convert_image(input_file.to_string(), ext);
+    // convert_image(input_file.to_string(), ext);
+
+    let application = Application::builder()
+        .application_id("com.def1de.CrossConvert")
+        .build();
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("First GTK Program")
+            .default_width(350)
+            .default_height(70)
+            .build();
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        window.set_child(Some(&button));
+
+        window.present();
+    });
+
+    application.run()
 }
