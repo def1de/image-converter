9a6d93fdb16df6b02a15acfea3a147d56a0ab3f5
Illia Katerynych <iliakat.kr@gmail.com>
2024-06-01 23:41:55 +0100

    gtk gui deprecated | back to a consle app

--- src/img_manager.rs
+++ src/img_manager.rs
@@ -1,48 +1,16 @@
-use gtk4 as gtk;
-use gtk::prelude::*;
 use image;
 use std::io::Error;
 use std::sync::{Arc, Mutex};
 
 const IMAGES: [&str; 11] = [
-    "jpg",
-    "avif",
-    "bmp",
-    "gif",
-    "png",
-    "pnm",
-    "qoi",
-    "tga",
-    "tiff",
-    "webp",
-    "ico"
+    "jpg", "avif", "bmp", "gif", "png", "pnm", "qoi", "tga", "tiff", "webp", "ico",
 ];
 
 pub fn is_image_extension(file_ext: String) -> bool {
     IMAGES.contains(&file_ext.to_lowercase().as_str())
 }
 
-pub fn create_app_buttons(vbox: &gtk::Box, input_file: String) -> Result<(), Error> {
-    println!("Creating buttons");
-    for i in 0..IMAGES.len() {
-        let button_label = format!("Convert to .{}", IMAGES[i]);
-        let button = gtk::Button::builder().label(&button_label).margin_bottom(12).build();
-        let input = input_file.clone();
-        let vbox_clone = vbox.clone();
-        button.connect_clicked(move |_| {
-            if let Err(_) = convert_image(input.clone(), &format!(".{}", IMAGES[i])){
-                let label = gtk::Label::builder().label("Error Saving Image").build();
-                vbox_clone.append(&label);
-            } else {
-                std::process::exit(0);
-            }
-        });
-        vbox.append(&button);
-    }
-    Ok(())
-}
-
-fn convert_image(input_file: String, ext: &str) -> Result<(), Error>{
+pub fn convert_image(input_file: String, ext: &str) -> Result<(), Error> {
     // Get the name of the image file
     println!("Converting image");
     let new_file_path: &str = input_file.split(".").collect::<Vec<&str>>()[0];
@@ -55,9 +23,19 @@ fn convert_image(input_file: String, ext: &str) -> Result<(), Error>{
             // Save the image file with required extension
             match img.save(new_path) {
                 Ok(_) => return Ok(()),
-                Err(e) => {return Err(Error::new(std::io::ErrorKind::Other, "Error reading image file"));}
+                Err(e) => {
+                    return Err(Error::new(
+                        std::io::ErrorKind::Other,
+                        "Error reading image file",
+                    ));
+                }
             }
         }
-        Err(e) => {return Err(Error::new(std::io::ErrorKind::Other, "Error opening image file"));}
+        Err(e) => {
+            return Err(Error::new(
+                std::io::ErrorKind::Other,
+                "Error opening image file",
+            ));
+        }
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -2,83 +2,42 @@
 #![allow(unused)]
 
 mod img_manager;
+use img_manager::{convert_image, is_image_extension};
 
 use std::time::Duration;
 
-use gtk4 as gtk;
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, gio::ApplicationFlags};
-use gtk::{Box, Orientation};
-
-struct File{
+struct File {
     path: String,
-    ext: String
+    ext: String,
 }
 
 fn get_file_path() -> File {
     // Get the input file path
     let args: Vec<String> = std::env::args().collect();
-    
+
     let input_file = args[1].to_string();
     let input_file = input_file.trim();
 
     //Get the extension of the input file
-    let ext: &str = match input_file.split(".").collect::<Vec<&str>>().last(){
+    let ext: &str = match input_file.split(".").collect::<Vec<&str>>().last() {
         Some(e) => e,
-        None => ""
+        None => "",
     };
 
-    return File{
+    return File {
         path: input_file.to_string(),
-        ext: ext.to_string()
-    }
+        ext: ext.to_string(),
+    };
 }
 
-fn main() -> glib::ExitCode{
-    
-    let application = Application::builder()
-        .application_id("com.def1de.CrossConvert")
-        .flags(ApplicationFlags::HANDLES_OPEN)
-        .build();
-
-    application.connect_open(|app, files, _| {
-        // Get the input file path
-        let file = get_file_path();
-
-        // Create the main window
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("Cross Convert")
-            .default_width(350)
-            .default_height(70)
-            .build();
-
-        // Create a vertical box layout
-        let vbox = Box::builder().orientation(Orientation::Vertical)
-        .margin_top(20)
-        .margin_bottom(20)
-        .margin_start(20)
-        .margin_end(20)
-        .build();
-        window.set_child(Some(&vbox));
-
-        let title_text = gtk::Label::builder()
-            .label("Select the format to convert the image to:")
-            .margin_bottom(12)
-            .build();
-        vbox.append(&title_text);
-
-        if img_manager::is_image_extension(file.ext) {
-            img_manager::create_app_buttons(&vbox, file.path.clone());
-        } else {
-            let error_text = gtk::Label::builder()
-                .label("This file format is not supported")
-                .margin_bottom(12)
-                .build();
-            vbox.append(&error_text);
+fn main() {
+    let file = get_file_path();
+    if is_image_extension(file.ext) {
+        match convert_image(file.path, ".png") {
+            Ok(_) => println!("Image converted successfully"),
+            Err(e) => println!("Error converting image: {:?}", e),
         }
-        window.present();
-    });
-
-    application.run_with_args(&std::env::args().collect::<Vec<_>>())
+    } else {
+        println!("Invalid image file extension");
+    }
 }
